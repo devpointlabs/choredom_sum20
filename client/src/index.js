cd61087bf3814e64ed317163ab4d984d1e9520b2
Ernie Rimer <ernest.rimer@icloud.com>
2020-06-29 23:08:33 -0600

    initial shell needs models to finish

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './providers/AuthProvider';
+import { initMiddleware } from 'devise-axios';
+
+initMiddleware()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
