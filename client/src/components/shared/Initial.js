285e7259a997314662459c2163657f22485505a4
Henry Doan <henrydoan96@gmail.com>
2020-07-08 21:36:13 -0600

    Update Initial.js

--- client/src/components/shared/Initial.js
+++ client/src/components/shared/Initial.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, } from 'react-router-dom';
 import { Header, Button,  } from "semantic-ui-react";
 import FamForm from "../fams/FamForm";
-import { Link, } from 'react-router-dom';
 
 const Initial = () => (
   <>
