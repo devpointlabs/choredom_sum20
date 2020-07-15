0653fd121edfc1da03d0bb18b941d43d911c1b54
Eva Ivanova <rilabg89@gmail.com>
2020-07-14 20:47:05 -0600

    Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  fam = Fam.create(
+    fam_name: Faker::Food.dish, 
+    fam_admins: "Admin", 
+    fam_members: "CoolKids"
+  )  
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password:"password",
+    points: Faker::Number.non_zero_digit,
+    admin: Faker::Boolean
+  )  
+  Task.create(
+    task_name:"task name",
+    task_description:"task description",
+    task_value: Faker::Number.non_zero_digit,
+    task_complete: Faker::Boolean,
+    user_id: user.id
+  )  
+  Reward.create(
+    reward_name: "reward name",
+    reward_description: "reward description",
+    reward_cost: Faker::Number.non_zero_digit,
+    reward_claimed: Faker::Boolean,
+    reward_used: Faker::Boolean,
+    user_id: user.id
+  )  
+  FamGroup = Famgroup.create(
+    last_name: Faker::Food.dish,
+    fam_id: fam.id,
+    user_id: user.id
+  )
+end
+puts "Data Seeded."
