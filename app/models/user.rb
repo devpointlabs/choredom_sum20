19a85d9eac2f89f09d759449617dd92ce2deff09
chadol <owner@Owners-MacBook-Pro.local>
2020-07-16 18:01:51 -0700

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   has_many  :rewards, dependent: :destroy
   extend Devise::Models
 
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
