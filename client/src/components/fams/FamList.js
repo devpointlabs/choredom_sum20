04437138d7d3e4fd774b2515a9495b530b7de11d
Ernie Rimer <ernest.rimer@icloud.com>
2020-07-16 18:26:35 -0600

    fam cards

--- client/src/components/fams/FamList.js
+++ client/src/components/fams/FamList.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Card, Header } from 'semantic-ui-react';
 
 const FamList = ({ fams }) => (
   <>
-    <h1>Families:</h1>
+    {/* <h1>Families:</h1> */}
+    <Grid columns={5} padded doubling>
     {
       fams.map( f => 
         <>
-          <Link to={{
-            pathname: `/fams/${f.id}`,
-            //
-            // pathname: `/famsgroups/${f.id}`,
-            state: {...f}
-          }}>
-            { f.fam_name }
-          </Link>
-          <br />
+        <Grid.Column>
+          <Card>
+            <Card.Content>
+              <br/>
+                <Header>
+                  <Link to={{
+                    pathname: `/fams/${f.id}`,
+                    state: {...f}
+                  }}>
+                    { f.fam_name }
+                  </Link>
+                </Header>
+              <br/>
+              <br/>
+              </Card.Content>
+          </Card>
+        </Grid.Column>
         </>
       )
     }
+    </Grid>
   </>
 )
 
 export default FamList;
+
+{/* <Grid columns={5} padded doubling>
+{
+  fams.map( f => 
+    <>
+    <Grid.Column>
+      <Card>
+        <Link to={{
+          pathname: `/fams/${f.id}`,
+          //
+          // pathname: `/famsgroups/${f.id}`,
+          state: {...f}
+        }}>
+          { f.fam_name }
+        </Link>
+      </Card>
+    </Grid.Column>
+    </>
+  )
+}
+</Grid> */}
--- client/src/components/fams/FamShow.js
+++ client/src/components/fams/FamShow.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FamConsumer } from '../../providers/FamProvider';
-import { Button } from 'semantic-ui-react';
+import { Button, Card, Grid } from 'semantic-ui-react';
 import FamForm from './FamForm';
 
 class FamShow extends Component {
@@ -14,10 +14,17 @@ class FamShow extends Component {
     const { updateFam, deleteFam, history } = this.props
     return(
       <>
-        <h1>{fam_name}</h1>
-        <h3>{fam_admins}</h3>
-        <h4>{fam_members}</h4>
-        { editing ?
+      <Grid.Column>
+        <Card>
+          <Card.Content header={fam_name} />
+          <Card.Content>
+            {fam_admins}
+          </Card.Content>
+          <Card.Content extra>
+            {fam_members}
+          </Card.Content>
+          <Card.Content>
+          { editing ?
             <FamForm 
               user_id={user_id}  
               id={id}
@@ -36,6 +43,31 @@ class FamShow extends Component {
         <Button onClick={() => deleteFam(id, history)}>
           Delete
         </Button>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+        {/* <h1>{fam_name}</h1>
+        <h3>{fam_admins}</h3>
+        <h4>{fam_members}</h4> */}
+        {/* { editing ?
+            <FamForm 
+              user_id={user_id}  
+              id={id}
+              fam_name={fam_name}
+              fam_admins={fam_admins}
+              fam_members={fam_members}
+              updateFam={updateFam}
+              toggleUpdate={this.toggleUpdate}
+              history={history}
+            />
+          :
+          <Button onClick={this.toggleUpdate}>
+            Edit
+          </Button>
+        }
+        <Button onClick={() => deleteFam(id, history)}>
+          Delete
+        </Button> */}
       </>
     )
   }
