42284732ff7566d12fadb5a7c9b4c2879e9a741e
Henry Doan <henrydoan96@gmail.com>
2020-07-20 20:42:56 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many  :rewards, dependent: :destroy
   validates :name, length: {minimum: 1 }
-  validates :age, length { is: 1}
-  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :name, presence: true
 
   extend Devise::Models
