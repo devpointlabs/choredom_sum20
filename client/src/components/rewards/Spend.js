3473d22392ab62e016091dfa891a07deb1ac2ffc
Henry Doan <henrydoan96@gmail.com>
2020-07-21 19:58:44 -0600

    Update Spend.js

--- client/src/components/rewards/Spend.js
+++ client/src/components/rewards/Spend.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Container, Button, Modal, Grid } from 'semantic-ui-react';
 import UserRewards from '../rewards/UserRewards'
 
-
-
 const Spend = () => (
   <>
     <h1>Spend Points</h1>
     <p> Spend points on rewards </p>
-      <Container>
-        < UserRewards />
-      </Container>
+    <Container>
+      <UserRewards />
+    </Container>
   </>
-       
-    
+)
 
-  )
 export default Spend;
