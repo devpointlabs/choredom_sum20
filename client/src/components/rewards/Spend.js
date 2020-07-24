ea53b4741117619b5f356ca3249c16bcb35afd9c
Eva Ivanova <rilabg89@gmail.com>
2020-07-24 10:04:07 -0600

    Styling for Pages

--- client/src/components/rewards/Spend.js
+++ client/src/components/rewards/Spend.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Container, Button, Modal, Grid } from 'semantic-ui-react';
 import UserRewards from '../rewards/UserRewards'
+import {Header} from 'semantic-ui-react'
+import styled from 'styledComp';
+
+const Container = styled.div`
+  background: linear-gradient(to top right, blue, gold);
+`
+<Segment basic>
+
+const Transparent = styled.div`
+  background: transparent !important;
+`
+<Segment as={Transparent}>
+
+const HeaderText = styled.h1`
+  color: black !important;
+  text-align: center;
+  width: 150px;
+  white-space nowrap;
+  overflow: hidden;
+  text-overflow: clip;
+`
 
 const Spend = () => (
   <>
-    <h1>Spend Points</h1>
-    <p> Spend points on rewards </p>
+  <div>
+    <h1 textAlign = "center"> Spend Points</h1>
+  <Segment>
+    <p textAlign="center"> Spend points on rewards </p>
     <Container>
       <UserRewards />
     </Container>
+  </Segment>
   </>
+  </div>
 )
 
 export default Spend;
--- client/src/components/tasks/Earn.js
+++ client/src/components/tasks/Earn.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Container, } from 'semantic-ui-react';
 import UserTasks from '../tasks/UserTasks'
+import {Header} from 'semantic-ui-react'
+import styled from 'styledComp';
+
+const Container = styled.div`
+  background: linear-gradient(to top right, pink, gold);
+`
+<Segment basic>
+
+const Transparent = styled.div`
+  background: transparent !important;
+`
+<Segment as={Transparent}>
+
+const HeaderText = styled.h1`
+  color: navy !important;
+  text-align: center;
+  width: 150px;
+  white-space nowrap;
+  overflow: hidden;
+  text-overflow: clip;
+`
 
 const Earn = () => (
   <>
-    <h1>Earn Points</h1>
-    <p> Complete tasks to earn points </p>
+  <div>
+    <h1 textAlign = "center"> Earn Points</h1>
+  <Segment>
+    <p textAlign="center"> Complete tasks to earn points </p>
       <Container>
         < UserTasks />
       </Container>
+  </Segment>
   </>
+  </div>
 )
 
 export default Earn;
