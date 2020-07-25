490324729c8fa838769b65c38df79d334f9fb36d
Ernie Rimer <ernest.rimer@icloud.com>
2020-07-25 08:46:11 -0600

    initiated styles

--- client/src/components/shared/AdminDash.js
+++ client/src/components/shared/AdminDash.js
@@ -1,22 +1,43 @@
 import React from 'react';
-import { Container, Button, Modal, Grid } from 'semantic-ui-react';
+import { Grid, Segment } from 'semantic-ui-react'
 import Fams from '../fams/Fams';
 import Rewards from '../rewards/Rewards';
 import Tasks from '../tasks/Tasks'
+import styled from 'styled-components';
+
+const fontSize = (size) => {
+  switch(size) {
+    case 'large':
+      return '24px';
+    case 'small':
+      return '14px';
+    default:
+      return '2rem';
+  }
+}
+
+const HeaderText = styled.h1`
+  color: black
+  text-align: center;
+  font-size: ${props => fontSize(props.fSize)} !important;
+`
 
 const AdminDash = () => (
   <>
-    <h1>Admin Dashboard</h1>
+    <p fontSize='14px' font-family='Work Sans'>My Family</p>
+    <HeaderText fSize="large">My Family</HeaderText>
     <p> My Family Section Across Top Row </p>
     <div>
       < Fams />
     </div>
-    <div>
-      < Rewards />
-    </div>
-    <div>
-      < Tasks />
-    </div>
+    <Grid columns='equal'>
+      <Grid.Column>
+        < Rewards />
+      </Grid.Column>
+      <Grid.Column>
+        < Tasks />
+      </Grid.Column>
+    </Grid>
   </>
 )
 
--- client/src/components/tasks/TaskList.js
+++ client/src/components/tasks/TaskList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
 
 const TaskList = ({ tasks }) => (
   tasks.map( t => 
     <>
+    <Segment>
       <Link to={{
         pathname: `/tasks/${t.id}`,
         state: {...t}
         }}>
         { t.task_name }
       </Link>
-      <br />
+      </Segment>
     </>
   )
 )
